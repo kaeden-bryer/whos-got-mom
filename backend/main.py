640ad7d9c50e7532957215a3f2bc60cbb44943df
Kaeden Bryer <130073365+kaeden-bryer@users.noreply.github.com>
2026-01-31 19:21:33 -0500

    Configure Supabase

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_ANON_KEY")
+
+# Use this client for standard user-scoped operations
+supabase: Client = create_client(url, key)
 
 app = FastAPI()
 
